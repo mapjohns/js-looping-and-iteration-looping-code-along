91a77f5beee452e3fd292e09817d11fd38e8d48c
Matthew <mattj20@gmail.com>
2022-10-07 20:58:51 -0700

    commit tree 776e51bd1728a0bdf6d9f19e4863c4bc91f809d1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let thankMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankMessages;
+}
+
+function countDown(pos) {
+    let i = pos;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
